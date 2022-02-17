beb700a42812100dec7938a941a817a72b293f32
Minasie Shibeshi <minasploit@gmail.com>
2022-02-17 19:55:12 +0300

    Revert "open chat automatically"
    This reverts commit 197e6cfdfca3150d62923936cab6f92a42fe96fe.

--- src/pages/Requests.js
+++ src/pages/Requests.js
@@ -2,13 +2,11 @@ import { useParams } from "react-router-dom"
 import { UseDocument } from "../hooks/useDocument"
 import RequestSummary from "./RequestSummary"
 import happy from "../components/undraw_Happy_announcement_re_tsm0.png"
-import { useHistory } from "react-router-dom"
 
 function Requests() {
 
     const { id } = useParams()
     const { document, error } = UseDocument('requests', id)
-    const history = useHistory()
 
     if (error) {
         return <div className="text-red-900">{error}</div>
@@ -26,7 +24,7 @@ function Requests() {
                 <span className="text-4xl text-primaryGreen font-bold">
                     Your Request Has Been Approved!
                 </span>
-                <div className="bg-primaryGreen rounded-md cursor-pointer p-3 mt-10" onClick={history.push("/")}>
+                <div className="bg-primaryGreen rounded-md cursor-pointer p-3 mt-10">
                     <span className="text-white">
                         Go to projects Dashboard
                     </span>
